ee28f67f57b16fb2e119791b49cf1a4ec0df8b09
Niesbert <niesbertveichenlaub@gmail.com>
2026-04-26 23:27:01 +0200

    Death events now work when touching tiles marked as deadly. There is a short delay before the player respawns.

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -23,3 +23,42 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	# Check every collision that happened during move_and_slide
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider is TileMapLayer or collider is TileMap:
+			# 1. Get the position where the hit happened
+			var hit_position = collision.get_position() - (collision.get_normal() * 4)
+			
+			# 2. Convert that world position to tile coordinates
+			var coords = collider.local_to_map(collider.to_local(hit_position))
+			
+			# 3. Get the data from that coordinate
+			var data: TileData
+			if collider is TileMapLayer:
+				data = collider.get_cell_tile_data(coords)
+			else:
+				# If using old TileMap, we check the first 3 layers for death
+				for layer in collider.get_layers_count():
+					data = collider.get_cell_tile_data(layer, coords)
+					if data: break 
+
+			# 4. NOW check the data
+			if data:
+				var deadly = data.get_custom_data("is_deadly")
+				if deadly:
+					die()
+
+func _ready() -> void:
+	# Tell the GameManager: "This is where I should come back to"
+	GameManager.respawn_point = global_position
+	add_to_group("player") # Helps the GameManager find the player node later
+
+func die():
+	# Stop movement so the player doesn't keep sliding while 'dead'
+	set_physics_process(false)
+	print("Death Triggered!")
+	# We call the GameManager, but the delay happens there
+	GameManager.respawn_player()
--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -2,6 +2,7 @@ extends Node
 var currentLevel = 1
 var LevelPath = "res://Scenes/Levels/Levels/"
 var coinsCollected = 0
+var respawn_point: Vector2 = Vector2.ZERO
 
 func ready():
 	resetCoins()
@@ -26,3 +27,24 @@ func addCoin():
 	
 func resetCoins():
 	coinsCollected = 0
+	
+func respawn_player():
+	var player = get_tree().get_first_node_in_group("player")
+	player.visible = false
+	
+	print("Waiting to respawn...")
+	# 1. The Delay: Wait for 1.0 seconds
+	await get_tree().create_timer(0.50).timeout
+	player.visible = true
+	
+	
+	if player:
+		# 3. Move the player back to the start
+		player.global_position = respawn_point
+		
+		# 4. Reset velocity so they don't 'fly' into the respawn point
+	if player is CharacterBody2D:
+		player.velocity = Vector2.ZERO    
+		# 5. Re-enable physics so they can move again
+	player.set_physics_process(true)
+	print("Player respawned!")
